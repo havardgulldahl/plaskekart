a7a3ed39337021933eb4ce4e287e71ecf914b6ba
Håvard Gulldahl <havard@gulldahl.no>
2016-08-11 21:54:48 +0200

    Fleshing out Models.swift, adding also structs that is deserializable into NowCast with SWXMLHash

--- plaskekart/Models.swift
+++ plaskekart/Models.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SWXMLHash
 
 class Location {
     // MARK: properties
@@ -28,7 +29,71 @@ struct LocationCast {
     // MARK: properties
     
     var loc: Location
-    var nowCasts: Array<AnyObject>?
+    var nowCasts: Array<NowCast>?
     var regionRadarMap: NSURL?
+}
+
+struct Precipitation {
+    let unit: String
+    let value: Float
     
+    init?(unit: String, value: Float) {
+        self.unit = unit
+        self.value = value
+        
+        if unit.isEmpty {
+            return nil
+        }
+    }
+}
+
+extension NSDate: XMLAttributeDeserializable  {
+    public static func deserialize(attribute: XMLAttribute) throws -> Self {
+        if attribute.text.isEmpty {
+            throw XMLDeserializationError.NodeHasNoValue
+        }
+        
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+        let date = dateFormatter.dateFromString(attribute.text)
+        
+        guard let validDate = date else {
+            throw XMLDeserializationError.AttributeDeserializationFailed(type: "Date", attribute: attribute)
+        }
+        
+        // NOTE THIS
+        return value(validDate)
+    }
+    
+    // AND THIS
+    private static func value<T>(date: NSDate) -> T {
+        return date as! T
+    }
+}
+
+
+struct NowCast: XMLIndexerDeserializable {
+    // a struct that is fit to deserialize a nowcast structure with SWXMLHash
+    // see https://github.com/drmohundro/SWXMLHash
+/* <time datatype="forecast" from="2016-08-10T22:30:00Z" to="2016-08-10T22:30:00Z">
+ <location latitude="60.1000" longitude="9.5800">
+ <precipitation unit="mm/h" value="0.0"/>
+ </location>
+ </time>
+ */
+    let timeFrom: NSDate
+    let timeTo: NSDate
+    let location: Location
+    let cast: Precipitation
+    
+    static func deserialize(node: XMLIndexer) throws -> NowCast {
+        return try NowCast(
+            timeFrom: node.value(ofAttribute: "from"),
+            timeTo: node.value(ofAttribute: "to"),
+            location: Location(latitude: node["location"].value(ofAttribute: "latitude"),
+                longitude: node["location"].value(ofAttribute: "longitude"))!,
+            cast: Precipitation(unit: node["precipitation"].value(ofAttribute: "unit"),
+                value: node["precipitation"].value(ofAttribute: "value"))!
+        )
+    }
 }
