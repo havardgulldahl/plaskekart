b72808b2f71aaa7a4ff84488caed5bd6e0117f5d
Håvard Gulldahl <havard@gulldahl.no>
2016-09-13 23:15:25 +0200

    Use LocationDelegate singleton

--- plaskekart/NowCastViewController.swift
+++ plaskekart/NowCastViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 import CoreLocation
 //import Haneke
 
-class NowCastViewController: UIViewController, CLLocationManagerDelegate, UICollectionViewDataSource, UICollectionViewDelegate {
+class NowCastViewController: UIViewController, LocationServiceDelegate, UICollectionViewDataSource, UICollectionViewDelegate {
 
     @IBOutlet weak var CollectionView: UICollectionView!
     @IBOutlet weak var Summary: UILabel!
@@ -24,67 +24,24 @@ class NowCastViewController: UIViewController, CLLocationManagerDelegate, UIColl
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // set up location manager (GPS) stuff
-        self.locationManager = CLLocationManager()
-        self.locationManager.requestWhenInUseAuthorization() // TODO: ask nicely first
-        
-        print("nowcastvc: ")
-        debugPrint(locationCast)
-        
-        if CLLocationManager.locationServicesEnabled() {
-            self.locationManager.delegate = self
-            self.locationManager.desiredAccuracy = kCLLocationAccuracyThreeKilometers
-            self.locationManager.startUpdatingLocation()
-        }
-        // Do any additional setup after loading the view.
-    }
-    // MARK: Location aware implementation
-    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
-        print("new location atuh status: \(status)")
-        var message: String = ""
-        switch status {
-        case CLAuthorizationStatus.Restricted:
-            // "Restricted Access to location"
-            message = NSLocalizedString("CLAuthorizationStatus.Restricted",
-                                        value:"Location access is restricted. Please go to settings to re-enable it",
-                                        comment:"User alert")
-        case CLAuthorizationStatus.Denied:
-            // "User denied access to location"
-            message = NSLocalizedString("CLAuthorizationStatus.Denied",
-                                        value:"Access to current location is needed for automatic maps. If you want to re-enable this, please go to settings",
-                                        comment:"User alert")
-            
-        case CLAuthorizationStatus.NotDetermined:
-            // "Status not determined"
-            message = NSLocalizedString("CLAuthorizationStatus.NotDetermined",
-                                        value:"Could not read Location. Please try again later",
-                                        comment:"User alert")
-            
-        default:
-            self.locationManager.startUpdatingLocation()
-            
-        }
-        if !message.isEmpty{
-            showAlert(message, vc:self)
-        }
-        
+        // set up location manager (GPS) Singleton
+        LocationService.sharedInstance.delegate = self
+        LocationService.sharedInstance.startUpdatingLocation()
     }
     
-    
-    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        // Get a fix on the user's location
-        //self.locationManager.stopUpdatingLocation()
+    func tracingLocation(currentLocation: CLLocation){
         
-        // get correct map from coordinates
-        let latestLocation = locations.last
-        
-        let latitude = String(format: "%.4f", latestLocation!.coordinate.latitude)
-        let longitude = String(format: "%.4f", latestLocation!.coordinate.longitude)
+        let latitude = String(format: "%.4f", currentLocation.coordinate.latitude)
+        let longitude = String(format: "%.4f", currentLocation.coordinate.longitude)
         
         self.updateNowCast(latitude, long: longitude)
-        
+    
     }
-
+    
+    func tracingLocationDidFailWithError(error: NSError) {
+        print("tracing Location Error : \(error.description)")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
